974afe4a78a1ded444e637066bfe1c920db2d48f
DESKTOP-OM1G0NG\Mads <mahosila@gmail.com>
2017-11-20 10:19:31 +0100

    Initiel Oversigt oprettele. Basis html

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-const express = require("express")
-const mongoose = require("mongoose")
+const express = require("express");
+const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/super-octo-spoon")
-mongoose.Promise = global.Promise
+mongoose.connect("mongodb://localhost:27017/super-octo-spoon");
+mongoose.Promise = global.Promise;
 
-const app = express()
+const app = express();
 
-app.get("/", (req,res) => res.end("HELLO WORLD"))
+//app.get("/", (req,res) => res.end("HELLO WORLD"))
 
-app.listen(1024)
+app.listen(1024);
+
+//Mainview
+//Headers
+//Menu, Søgeknap
+//#Headers
+//#Data
