ec5321f02200e29eb3c4c75690a5732fb367a90f
Mads A. H. Jensen <Mads-AH-Jensen@live.dk>
2017-11-20 14:00:30 +0100

    Finished large part of GUI for US-something

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-const express = require("express");
+const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
+const express = require("express");
 const hbs = require("hbs");
 
 mongoose.connect("mongodb://localhost:27017/super-octo-spoon");
 mongoose.Promise = global.Promise;
 
+var Order = require('./models/order');
+
 const app = express();
 app.set('view engine', 'hbs');
+
+app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 const testOrders = [
     {
@@ -48,5 +56,32 @@ const testOrders = [
 ];
 
 app.get("/", (req,res) => res.render("overview", { orders: testOrders }));
+
+app.get("/opretOrdre", (req,res) => res.sendFile(__dirname + "/views/createOrder.html"));
+
+app.post("/opretOrdre", (req, res) => {
+    if(req.body.landlineNumber || req.body.phoneNumber) {
+        try{
+            var order = new Order({
+                consultant:     req.body.consultant,
+                signedDate:     req.body.signedDate,
+                landlineNumber: req.body.landlineNumber,
+                phoneNumber:    req.body.phoneNumber,
+                name:           req.body.name,
+                address:        req.body.address,
+                comment:        req.body.comment
+            });
+
+            order.save().then(() => res.json({message: "Ordre oprettet i database."})).catch(() => res.json({error: "Ordre kunne ikke oprettes i database."}));
+
+        }catch(e){
+            console.log(e);
+            res.json({error: "Ordre kunne ikke oprettes i database."});
+        }
+    }else{
+        res.json({error: "Intet telefonnummer angivet."});
+    }
+});
+
 
 app.listen(1024);
