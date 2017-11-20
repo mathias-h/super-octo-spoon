6c3e2d49807668800998a81c85e9965789e12239
mathias hansen <mathiasosthan@gmail.com>
2017-11-20 10:16:09 +0100

    added test data

---
+++ test/test-data.js
@@ -0,0 +1,38 @@
+const testOrders = [
+    {
+        consultant: "CONSULANT1",
+        createdDate: new Date("1/1/2017"),
+        tlf: "88888888",
+        name: "NN1 Bondegården",
+        address: {
+            street: "Markvejen 1",
+            city: "Bondeby",
+            zip: "8123"
+        },
+        comment: "Ring efter høst"
+    },
+    {
+        consultant: "CONSULANT1",
+        createdDate: new Date("1/1/2017"),
+        tlf: "88888889",
+        name: "NN1 Bondegården",
+        address: {
+            street: "Markvejen 2",
+            city: "Bondeby",
+            zip: "8123"
+        },
+        comment: "Ring før høst"
+    },
+    {
+        consultant: "CONSULANT2",
+        createdDate: new Date("1/1/2017"),
+        tlf: "88888888",
+        name: "NN1 Bondegården",
+        address: {
+            street: "Markvejen 3",
+            city: "Bondeby",
+            zip: "8123"
+        },
+        comment: "Ring under høst"
+    }
+]
