560aff3611df3694ab7424cb34c51e2ed7f0b28b
Nicolai K <eaaniklo@students.eaaa.dk>
2017-11-20 22:38:36 +0100

    Sorting  - tests færdig indtil mere data + TeamCity tests

--- models/sort.js
+++ models/sort.js
@@ -9,13 +9,23 @@ module.exports.sort = function (orders, sortBy) {
         return _.orderBy(orders, function (order) {
             return order.consultant
         });
-    }else if (sortBy == "xxx"){
+    }else if (sortBy == "date"){
+        return _.orderBy(orders, function (order) {
+            return order.createdDate
+        });
+    }else if (sortBy == "name") {
+        //Requires at least 2 names!!
+        //TODO Safe requirement or better validation?
+        return _.orderBy(orders, function (order) {
+            var names = order.name.split(" ");
+            //var firstName = names[0];
+            //var lastName = names[names.length-1]
+            return names[names.length-1] + names[0];
+        });
 
     }else if (sortBy == "xxx"){
 
     }else if (sortBy == "xxx"){
 
-    }else if (sortBy == "xxx"){
-
-    };
+ }
 }
