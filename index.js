54dcc56dc1bd676249748f67f2c27004892f3495
Nicolas Damgaard Larsen <ndlarsen@gmail.com>
2017-11-23 09:46:21 +0100

    asd

--- index.js
+++ index.js
@@ -4,53 +4,58 @@ const mongoose = require("mongoose");
 const moment = require("moment");
 const hbs = require("hbs");
 
+const CONSULTANTS = ["MH","MJ","NK","NL","MHL"]
+
 const { search } = require("./models/search");
 
 mongoose.connect("mongodb://localhost:27017/super-octo-spoon");
 mongoose.Promise = global.Promise;
 
-var Order = require('./models/order');
+const Order = require('./models/order');
 
 const app = express();
 app.set('view engine', 'hbs');
 
 app.use(express.static(__dirname + '/public'));
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.get("/", (req,res) => {
-    Order.find({}).lean().exec((err, orders) => {
-        const query = req.query.query
-        console.log(Object.assign({ a: 1 }, orders[0]))
+    Order.find().sort({ signedDate: -1 }).lean().exec((err, orders) => {
+        const query = req.query.query;
         const data = {
             orders: search(orders, query)
-                .map(o => {
-                    o.signedDate = moment(o.signedDate).format("DD-MM-YYYY")
-                    return o
-                }),
-            query
-        }
+                .map(o => Object.assign(o, { signedDate: moment(o.signedDate).format("DD-MM-YYYY") })),
+            query,
+            consultants: CONSULTANTS
+        };
         res.render("overview", data)
     })
 });
 
-app.get("/opretOrdre", (req,res) => res.sendFile(__dirname + "/views/createOrder.html"));
-
-app.post("/opretOrdre", (req, res) => {
+app.post("/order/create", (req, res) => {
+    console.log("POST");
     if(req.body.landlineNumber || req.body.phoneNumber) {
         try {
-            var order = new Order({
-                consultant:     req.body.consultant,
-                signedDate:     req.body.signedDate,
+            const order = new Order({
+                consultant: req.body.consultant,
+                signedDate: req.body.signedDate,
                 landlineNumber: req.body.landlineNumber,
-                phoneNumber:    req.body.phoneNumber,
-                name:           req.body.name,
-                address:        req.body.address,
-                comment:        req.body.comment
+                phoneNumber: req.body.phoneNumber,
+                name: req.body.name,
+                address: {
+                    street: req.body.street,
+                    city: req.body.city,
+                    zip: req.body.zip
+                },
+                comment: req.body.comment
             });
 
-            order.save().then(() => res.json({message: "Ordre oprettet i database."})).catch(() => res.json({error: "Ordre kunne ikke oprettes i database."}));
+            order.save().then(() => res.json({message: "Ordre oprettet i database."}))
+                .catch((e) => {
+                    res.json({error: "Ordre kunne ikke oprettes i database."});
+                    console.log(e);
+                });
 
         } catch(e) {
             console.log(e);
@@ -61,5 +66,24 @@ app.post("/opretOrdre", (req, res) => {
     }
 });
 
+app.get("/order/:orderId", (req,res) => {
+    const orderId = req.params.orderId;
+    Order.findOne({ _id: orderId }).then(order => {
+        if (!order) res.status(404).send("order not found");
+        else res.json(order)
+    })
+});
+
+app.post("/order", (req,res) => {
+    const order = req.body;
+
+    Order.findOneAndUpdate({ _id: order._id }, { $set: order }).then(() => {
+        res.end("order updated")
+    }).catch(err => {
+        res.status(500).json(err)
+    })
+});
 
 app.listen(1024);
+
+module.exports.app = app;
