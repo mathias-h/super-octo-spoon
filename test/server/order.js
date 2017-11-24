67ef1952813b8e86dfd58ad68965c8fc5e660915
DESKTOP-OM1G0NG\Mads <mahosila@gmail.com>
2017-11-24 10:40:53 +0100

    Koderettelser, samt tests og CSS.

--- test/server/order.js
+++ test/server/order.js
@@ -5,36 +5,36 @@ const { createApp } = require("../../app");
 describe("Order integration tests", () => {
     describe("getOrder", () => {
         it("should get order", () => {
-            const orderId = "ORDER_ID"
+            const orderId = "ORDER_ID";
             const order = {
                 _id: orderId,
                 order: "order"
-            }
+            };
             const OrderMock = { 
                 findOne(query) {
-                    expect(query._id).to.eq(orderId)
+                    expect(query._id).to.eq(orderId);
                     return Promise.resolve(order)
                 }
-            }
-            const app = createApp(OrderMock)
+            };
+            const app = createApp(OrderMock);
 
             return request(app)
                 .get("/order/" + orderId)
                 .expect("Content-Type", /application\/json/)
                 .expect(200)
                 .expect(JSON.stringify(order))
-        })
+        });
 
         it("should handle nonexistant order", () => {
-            const orderId = "NONEXISTANT_ORDER_ID"
+            const orderId = "NONEXISTANT_ORDER_ID";
             const order = {
                 _id: orderId
-            }
+            };
             const OrderMock = { 
                 findOne: () => Promise.resolve(null)
-            }
+            };
 
-            const app = createApp(OrderMock)
+            const app = createApp(OrderMock);
 
             return request(app)
                 .get("/order/" + orderId)
@@ -42,21 +42,21 @@ describe("Order integration tests", () => {
                 .expect(404)
                 .expect("order not found")
         })
-    })
+    });
     it("edit order", () => {
-        const orderId = "ORDER_ID"
+        const orderId = "ORDER_ID";
         const order = {
             _id: orderId,
             order: "order"
-        }
+        };
         const OrderMock = { 
             editOrder(o) {
-                expect(o).to.deep.eq(order)
+                expect(o).to.deep.eq(order);
                 return Promise.resolve()
             }
-        }
+        };
         
-        const app = createApp(OrderMock)
+        const app = createApp(OrderMock);
 
         return request(app)
             .post("/order")
@@ -65,4 +65,4 @@ describe("Order integration tests", () => {
             .expect(200)
             .expect("order updated")
     })
-})
+});
