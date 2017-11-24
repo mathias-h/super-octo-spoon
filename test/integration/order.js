aac9efe43088aa22837f1538e3fd4e34c8cc8af5
mathias hansen <mathiasosthan@gmail.com>
2017-11-24 12:56:14 +0100

    added integration tests

--- test/integration/order.js
+++ test/integration/order.js
@@ -3,87 +3,80 @@ const childProcess = require("child_process")
 const mongoose = require("mongoose")
 const { expect } = require("chai")
 const fs = require("fs")
+const rimraf = require("rimraf")
+const moment = require("moment")
 
 const { Order : OrderSchema } = require("../../models/order")
 const { createApp } = require("../../app")
 
 const sleep = time => new Promise(resolve => setTimeout(resolve, time))
 
-describe("Order integration tests", () => {
-    let dbProcess
+describe("order integration test", () => {
     let server
+    let db
     let browser
-    const Order = mongoose.model("Order", OrderSchema)
-    before(() => {
-        const dataPath = __dirname + "/../test-data"
-        childProcess.execSync("rm -rf " + dataPath)
-        fs.mkdir(dataPath)
+    let OrderModel = mongoose.model("Order", OrderSchema)
 
-        dbProcess = childProcess.spawn("mongod", ["--dbpath", dataPath])
+    before(async () => {
+        const dataPath = __dirname + "/../test-data"
+        rimraf.sync(dataPath)
+        fs.mkdirSync(dataPath)
+
+        db = childProcess.spawn("mongod", ["--dbpath", dataPath])
+        
+        await sleep(10)
+
+        mongoose.connect("mongodb://localhost:27017/super-octo-spoon");
         mongoose.Promise = global.Promise;
 
-        return sleep(200)
-            .then(() => mongoose.connect("mongodb://localhost:27017/super-octo-spoon"))
-            .then(() => {
-                app = createApp(Order);
-                server = app.listen(1024)
-            }).then(() => puppeteer.launch()).then(b => browser = b)
+        server = createApp(OrderModel).listen(1024)
+        browser = await puppeteer.launch({ headless: false, devtools: true })
     })
 
-    after(() => {
-        server.close();
-        return mongoose.disconnect()
-            .then(() => dbProcess.kill())
-            .then(() => browser.close());
+    after(async () => {
+        server.close()
+        db.kill()
+        browser.close()
     })
 
-    describe("overview tests", () => {
+    beforeEach(() => OrderModel.remove({}).exec())
+
+    it("should show orders in overview", async () => {
         const orderId1 = mongoose.Types.ObjectId()
-        const orderId2 = mongoose.Types.ObjectId()
-
-        beforeEach(() => {
-            const order1 = new Order({
-                _id: orderId1,
-                consultant: "MH",
-                signedDate: new Date(),
-                landlineNumber: "88888888",
-                phoneNumber: "88888888",
-                name: "NAME",
-                farmName: "FARM_NAME",
-                address: {
-                    city: "CITY",
-                    street: "STREET",
-                    zip: "9999"
-                }
-            })
-            const order2 = new Order({
-                _id: orderId2,
-                consultant: "MH",
-                signedDate: new Date(),
-                landlineNumber: "88888888",
-                phoneNumber: "88888888",
-                name: "NAME",
-                farmName: "FARM_NAME",
-                address: {
-                    city: "CITY",
-                    street: "STREET",
-                    zip: "9999"
-                }
-            })
-
-            return Order.remove().exec()
-                .then(() => order1.save())
-                .then(() => order2.save())
+        orderId1.toHexString()
+        const order = new OrderModel({
+            _id: orderId1,
+            consultant: "CONTULTANT",
+            signedDate: new Date(),
+            name: "NAME",
+            farmName: "FARM_NAME",
+            address: {
+                city: "CITY",
+                street: "STREET",
+                zip: 9999
+            }
         })
-
-        it("should get orders", () =>
-            browser.newPage().then(page => page.goto("http://localhost:1024")
-                .then(() =>
-                    page.evaluate(function () {
-                        return Array.from(document.querySelectorAll("tr.order"))
-                            .map(orderEl => orderEl.getAttribute("data-order-id"))
-                    })
-                    .then(orderIds => expect(orderIds).to.deep.eq([orderId1.toHexString(),orderId2.toHexString()]))
-                    .then(() => page.close()))))
+        const orderId2 = mongoose.Types.ObjectId()
+        const order1 = new OrderModel({
+            _id: orderId2,
+            consultant: "CONTULTANT",
+            signedDate: new Date(),
+            name: "NAME",
+            farmName: "FARM_NAME",
+            address: {
+                city: "CITY",
+                street: "STREET",
+                zip: 9999
+            }
+        })
+        await order.save()
+        await order1.save()
+        const page = await browser.newPage()
+        await page.goto("http://localhost:1024/")
+        const orderIds = await page.evaluate(() =>
+            Array.from(document.querySelectorAll("tr.order"))
+                .map(o => o.getAttribute("data-order-id")))
+        expect(orderIds).to.deep.eq([orderId1.toHexString(), orderId2.toHexString()])
+        await page.close()
     })
 })
