4927db93c3a84d891771e06fb619ae4c9604b7ef
DESKTOP-OM1G0NG\Mads <mahosila@gmail.com>
2017-11-24 11:54:31 +0100

    Merge branch 'lordMadsBranch' of C:\Users\Mads\Desktop\super-octo-spoon with conflicts.

--- test/server/order.js
+++ test/server/order.js
@@ -5,36 +5,36 @@ const { createApp } = require("../../app");
 describe("Order integration tests", () => {
     describe("getOrder", () => {
         it("should get order", () => {
-            const orderId = "ORDER_ID";
+            const orderId = "ORDER_ID"
             const order = {
                 _id: orderId,
                 order: "order"
-            };
+            }
             const OrderMock = { 
                 findOne(query) {
-                    expect(query._id).to.eq(orderId);
+                    expect(query._id).to.eq(orderId)
                     return Promise.resolve(order)
                 }
-            };
-            const app = createApp(OrderMock);
+            }
+            const app = createApp(OrderMock)
 
             return request(app)
                 .get("/order/" + orderId)
                 .expect("Content-Type", /application\/json/)
                 .expect(200)
                 .expect(JSON.stringify(order))
-        });
+        })
 
         it("should handle nonexistant order", () => {
-            const orderId = "NONEXISTANT_ORDER_ID";
+            const orderId = "NONEXISTANT_ORDER_ID"
             const order = {
                 _id: orderId
-            };
+            }
             const OrderMock = { 
                 findOne: () => Promise.resolve(null)
-            };
+            }
 
-            const app = createApp(OrderMock);
+            const app = createApp(OrderMock)
 
             return request(app)
                 .get("/order/" + orderId)
@@ -42,21 +42,21 @@ describe("Order integration tests", () => {
                 .expect(404)
                 .expect("order not found")
         })
-    });
+    })
     it("edit order", () => {
-        const orderId = "ORDER_ID";
+        const orderId = "ORDER_ID"
         const order = {
             _id: orderId,
             order: "order"
-        };
+        }
         const OrderMock = { 
             editOrder(o) {
-                expect(o).to.deep.eq(order);
+                expect(o).to.deep.eq(order)
                 return Promise.resolve()
             }
         };
         
-        const app = createApp(OrderMock);
+        const app = createApp(OrderMock)
 
         return request(app)
             .post("/order")
@@ -64,5 +64,81 @@ describe("Order integration tests", () => {
             .expect("Content-Type", /text\/plain/)
             .expect(200)
             .expect("order updated")
+    });
+    describe("create order", () => {
+        it("should create order", () => {
+            const order = {
+                _id: "ORDER_ID",
+                consultant: "CONSULTANT",
+                signedDate: 2017-11-24,
+                landlineNumber: 11223344,
+                phoneNumber: 55667788,
+                name: 'NAME',
+                address: {
+                    street: 'STREET',
+                    city: 'CITY',
+                    zip: '1234'
+                },
+                comment: 'COMMENT'
+            };
+
+            const OrderMock = {
+                createOrder(o) {
+                    expect(o).to.deep.eq(order);
+                    return Promise.resolve();
+                }
+            };
+
+            const app = createApp(OrderMock);
+
+            return request(app)
+                .post('/order/create')
+                .send(order)
+                .expect('Content-Type', /text\/plain/)
+                .expect(200)
+                .expect('order created');
+        });
+
+        it('should handle create requests, which are missing all data', () => {
+            const order = {
+                _id: "ORDER_ID"
+            };
+
+            const OrderMock = {
+                createOrder(o) {
+                    expect(o).to.deep.eq(order);
+                    return Promise.reject();
+                }
+            };
+
+            const app = createApp(OrderMock);
+
+            return request(app)
+                .post('/order/create')
+                .send(order)
+                .expect('Content-Type', /application\/json/)
+                .expect(500);
+        });
+        it('should handle create requests, which are missing all data but phone number', () => {
+            const order = {
+                _id: "ORDER_ID",
+                landlineNumber: 11223344
+            };
+
+            const OrderMock = {
+                createOrder(o){
+                    expect(o).to.deep.eq(order);
+                    return Promise.reject();
+                }
+            };
+
+            const app = createApp(OrderMock);
+
+            return request(app)
+                .post('/order/create')
+                .send(order)
+                .expect('Content-Type', /application\/json/)
+                .expect(500)
+        });
     })
 });
