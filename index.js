68ee2a07bfba4b3121b16a61242fb70e61b6420f
mathias hansen <mathiasosthan@gmail.com>
2017-11-28 13:55:53 +0100

    changed to createConnection

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const {createApp} = require("./app");
 const { Order } = require("./models/order");
 const { User } = require("./models/user");
 
-//TODO Connection skal skrives om ved lejlighed så vi slipper for den deprecation warning.
-
-mongoose.connect("mongodb://localhost:27017/super-octo-spoon");
 mongoose.Promise = global.Promise;
+const connection = mongoose.createConnection("mongodb://localhost:27017/super-octo-spoon")
 
-const OrderModel = mongoose.model("Order", Order)
-const UserModel = mongoose.model('User', User)
+const OrderModel = connection.model("Order", Order)
+const UserModel = connection.model('User', User)
 
 const app = createApp(OrderModel, UserModel);
 
-app.listen(1024);
+app.listen(1024, () => {
+    console.log("listening")
+});
