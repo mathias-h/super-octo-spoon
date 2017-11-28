ba4c6e28e2684847d7d0905bf9bd0752f08e3a33
Nicolas Damgaard Larsen <ndlarsen@gmail.com>
2017-11-28 18:08:33 +0100

    user model tests wirting started

--- test/user.js
+++ test/user.js
@@ -1,8 +1,28 @@
 "use strict";
 
 const { expect } = require("chai");
-const { sort } = require("../models/sort");
+const mongoose = require('mongoose');
 
 describe("create user", function () {
+    let User;
+
+    beforeEach(function () {
+        User = require("../models/user");
+    });
+
+    it('should create user in database', function () {
+        const user = new User({
+            username: "testUser01",
+            password: "12345678",
+            isAdmin: false
+        });
+    });
+
+    it('should edit user', function () {
+        //TODO
+    });
+
+
+
 
 });
