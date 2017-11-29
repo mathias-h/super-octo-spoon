d24c45a54ed98074901a724a53622a8117b4eadb
Nicolas Damgaard Larsen <ndlarsen@gmail.com>
2017-11-29 19:56:43 +0100

    Minor changes to user test.
    Minor changes to user test. Still not complete.

--- test/user.js
+++ test/user.js
@@ -7,34 +7,40 @@ describe("user", function () {
     let User;
 
     beforeEach(function () {
-        User = require("../models/user");
+        User = require("../models/user").User;
     });
 
-    it('should create user in database', function () {
+    it('should create a user ', function () {
 
         const oldUserStatics = User.statics;
 
         User.statics = function(user){
             expect(user.username).to.eq(newUser.username);
+            // TODO: Nedenstående fejler nok pga. hashing
+            expect(user.password).to.eq(newUser.password);
+            expect(user.isAdmin).to.eq(newUser.isAdmin);
+            expect(user.isDisabled).to.eq(newUser.isDisabled);
 
             return User.statics;
         };
 
         Object.assign(User.statics, oldUserStatics);
 
-        const user = new User({
+        const newUser = {
             username: "testUser01",
             password: "12345678",
             isAdmin: true,
             isDisabled: false
-        });
+        };
 
         User.statics.save = function saveMock(){
             return {exec: function (){
                 return Promise.resolve()
                 }
             }
-        }
+        };
+
+        return User.statics.createUser(newUser);
     });
 
     it('should edit user', function () {
