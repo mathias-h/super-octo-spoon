ea0c935b78f267af1f4f7362938cc416e4506e17
Nicolas Damgaard Larsen <ndlarsen@gmail.com>
2017-11-29 13:42:05 +0100

    User model updated

--- models/user.js
+++ models/user.js
@@ -3,8 +3,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const bcrypt = require('bcrypt');
-const salt = require('./salt');
-const Salt = mongoose.model('Salt', salt);
 const SALT_ROUNDS = 10;
 
 const User = new Schema({
@@ -17,16 +15,6 @@ const User = new Schema({
         type: String,
         required: true
     },
-    // rewrite related
-    saltId: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Salt'
-    },
-    /*
-    salt: {
-        type: String
-    },
-    */
     isAdmin: {
         required: true,
         type: Boolean,
@@ -65,14 +53,16 @@ User.pre('save', function (next) {
         return next(new Error('Password is not a string.'));
     }
 
-    try {
-        this.salt = bcrypt.genSaltSync(SALT_ROUNDS);
-        this.password = bcrypt.hashSync(this.password, this.salt);
+    let that = this;
+
+    bcrypt.hash(this.password, SALT_ROUNDS, function (error, hash) {
+        if(error){
+            return next(error);
+        }
+
+        that.password = hash;
         return next();
-    }
-    catch(error){
-        return next(error);
-    }
+    });
 });
 
 User.pre('findOneAndUpdate', function (next) {
@@ -100,101 +90,109 @@ User.pre('findOneAndUpdate', function (next) {
         return next(new Error('Admin level is not a string.'));
     }
 
-    try {
-        const salt = bcrypt.genSaltSync(SALT_ROUNDS);
-        const hashedPassword = bcrypt.hashSync(password, salt);
-        this.getUpdate().$set.password = hashedPassword;
-        this.getUpdate().$set.salt = salt;
-        return next();
-    } catch (error) {
-        return next(error);
+    let that = this;
+    if(password){
+        bcrypt.hash(password, SALT_ROUNDS, function (error, hash) {
+            if(error){
+                return next(error);
+            }
+
+            that.getUpdate().$set.password = hash;
+            return next();
+        });
     }
+    return next();
 });
 
 User.statics.matchPasswords = function (username, password) {
+
     return this.findOne({username: username})
         .then(function (response) {
             if(!response){
                 return Promise.reject({status: "ERROR", message: "Username or password is invalid."});
             }
 
-            const hashedPassword = bcrypt.hashSync(password, response.salt);
-            if(hashedPassword !== response.password){
-                return Promise.reject({status: "ERROR", message: "Username or password is invalid."});
-            }
-
-            var result = {
+            const result = {
                 status: "OK",
                 message: "Authentication OK",
                 userData: {
+                    id: response._id,
                     username: response.username,
-                    isAdmin: response.isAdmin
+                    isAdmin: response.isAdmin,
+                    isDisabled: response.isDisabled
+                }
+            };
+
+            bcrypt.compare(password, response.password)
+                .then(function (isValidLogin) {
+
+                    if(isValidLogin){
+                        Promise.resolve(result);
                     }
-                };
-            return Promise.resolve(result);
-        })
-};
+                    else{
+                        Promise.reject({status: "ERROR", message: "Username or password is incorrect."});
+                    }
 
-// REWRITE STARTS HERE
-
-User.statics.createUser = function (userData){
-    if(!userData.username){
-        return Promise.reject(new Error('Username missing.'));
-    }
-    if(userData.username && typeof userData.username !== 'string'){
-        return Promise.reject(new Error('Username is not a string.'));
-    }
-    if(userData.username === ''){
-        return Promise.reject(new Error('Username is an empty string.'));
-    }
-    if(!userData.password){
-        return Promise.reject(new Error('Password missing'));
-    }
-    if(userData.password && typeof userData.password !== 'string'){
-        return Promise.reject(new Error('Password is not a string.'));
-    }
-    if(userData.password === ''){
-        return Promise.reject(new Error('Password is an empty string'));
-    }
-
-    const salt = new Salt(userData);
-
-    return salt.save()
-        .then(function () {
-            // TODO create/save user
-            // if user cannot be saved, delete salt from db
-            var user = new this({
-                username: userData.username,
-                password: bcrypt.hashSync(userData.password, salt.value),
-                saltId: salt._id
-            });
-            if(userData.isAdmin){
-                user.isAdmin = userData.isAdmin;
-            }
-            if(userData.isDisabled){
-                user.isDisabled = userData.isDisabled;
-            }
-            user.save()
-                .then(function (response) {
-                    console.log(response);
-                    return Promise.resolve('User saved to database')
                 })
                 .catch(function (error) {
-                    console.log(error);
-                    Salt.findByIdAndRemove(salt._id).then(function (response) {
-                        console.log(response);
-                        return Promise.reject(new Error('Could not save user to database.'));
-                    });
+                    Promise.reject({status: "ERROR", message: "Could not validate.", error: error});
                 });
         })
         .catch(function (error) {
-            console.log(error);
-            return Promise.reject(new Error('Could not save user to database.'));
+            return Promise.reject({status: "ERROR", message: "Could not validate.", error: error});
         });
 
 };
 
+User.statics.updateUser = function (userId, userData) {
 
-// REWRITE ENDS HERE
+    const condition = {
+        _id: userId
+    };
+
+    const update = {
+        $set: userData
+    };
+
+    return this.findOneAndUpdate(condition, update, {runValidators: true})
+        .then(function (response) {
+            console.log("DEBUG: findOneAndUpdate then():");
+            console.log(response);
+            if(response){
+                console.log("resolving");
+                return {status: "OK", message: "User updated."};
+            } else{
+                throw new Error("User not found.");
+            }
+        })
+
+};
+
+User.statics.createUser = function (userData) {
+
+    const user = new this();
+
+    if(userData.username){
+        user.username = userData.username;
+    }
+    if(userData.password){
+        user.password = userData.password;
+    }
+    if(userData.isAdmin){
+        user.isAdmin = userData.isAdmin;
+    }
+    if(userData.isDisabled){
+        user.isDisabled = userData.isDisabled;
+    }
+
+    return user.save()
+        .then(function (response) {
+            console.log(response);
+            return {status: "OK", message: "User created."};
+        }).catch(function (error) {
+            console.log(error);
+            throw new Error("Could not create user.");
+        });
+};
 
 module.exports.User = User;
