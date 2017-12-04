b47bd318affd9771344ba856aea4ed7f6945327f
Nicolas Damgaard Larsen <ndlarsen@gmail.com>
2017-12-04 09:10:18 +0100

    Remocved chai-http and stuff

--- test/login.js
+++ test/login.js
@@ -14,12 +14,58 @@ describe(' /GET on all end routes, session and login test', function () {
 });
 */
 
-process.env.NODE_ENV = 'test';
-
-const mongoose = require('mongoose');
-const User = require('../models/user');
-
-const chai = require('chai');
-const chaiHTTP = require('chai-http');
-
+const request = require('supertest');
+const testSession = require("supertest-session")
+const { expect } = require("chai");
 const { createApp } = require("../app");
+
+describe('login testing', function () {
+
+    function sessionMock(userId) {
+        return () => (req,res,next) => {
+            req.session = {
+                isLoggedIn: true,
+                userId: userId
+            };
+
+            next();
+        }
+    }
+
+    describe('GET /', function () {
+        it('should redirect to /login', function () {
+
+            const app = createApp({
+                session: sessionMock()
+            });
+
+            return request(app)
+                .get('/')
+                .expect()
+
+        })
+    });
+
+    describe('testing response when not logged in', function () {
+
+        it('GET / should redirect to GET /login', function () {
+
+        });
+
+        it('should fail at GET /', function () {
+
+        });
+
+        it('', function () {
+
+        })
+
+    });
+
+    describe('testing response when logged in', function () {
+
+        it('')
+
+    });
+
+});
