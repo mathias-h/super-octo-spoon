b737b7e6d8381607fa06d315edd80e4feef18cb5
Nicolas Damgaard Larsen <ndlarsen@gmail.com>
2017-12-08 18:11:42 +0100

    mere consultant test.

--- test/consultant.js
+++ test/consultant.js
@@ -66,8 +66,6 @@ describe("consultant tests", () => {
 
     beforeEach(async () => {
 
-        await Order.remove({});
-        await Season.remove({});
         await Consultant.remove({});
 
         consultantData = {
@@ -78,149 +76,6 @@ describe("consultant tests", () => {
         };
 
         await Consultant.createConsultant(consultantData);
-
-        const consultDataArr = [
-            {
-                "name": "superuser",
-                "password": "5up3r53cr37P455w0rd",
-                "isAdmin": true,
-                "dummy": false
-            },
-            {
-                "name": "admin",
-                "password": "admin",
-                "isAdmin": false,
-                "dummy": false
-            },
-            {
-                "name": "Slettet bruger",
-                "password": "dummy",
-                "isAdmin": false,
-                "dummy": true
-            },
-            {
-                "name": "MH",
-                "password": "MHPassword",
-                "isAdmin": false,
-                "dummy": false
-            },
-            {
-                "name": "MJ",
-                "password": "MJPassword",
-                "isAdmin": false,
-                "dummy": false
-            },
-            {
-                "name": "NL",
-                "password": "NLPassword",
-                "isAdmin": false,
-                "dummy": false
-            },
-            {
-                "name": "NK",
-                "password": "NKPassword",
-                "isAdmin": false,
-                "dummy": false
-            }
-        ];
-
-        const orderDataArr = [
-            {
-                "consultant": "MH",
-                "signedDate": new Date("1/1/2017"),
-                "landlineNumber": "88888888",
-                "phoneNumber": "20202020",
-                "name": "NN1",
-                "farmName": "Bondegården",
-                "address": {
-                    "street": "Markvejen 1",
-                    "city": "Bondeby",
-                    "zip": "8123"
-                },
-                "comment": "Ring efter høst",
-                "sampleDensity": 1,
-                "samePlanAsLast": true,
-                "takeOwnSamples": true,
-                "area": 100
-            },
-            {
-                "consultant": "MJ",
-                "signedDate": new Date("1/1/2017"),
-                "landlineNumber": "88888889",
-                "phoneNumber": "20202020",
-                "name": "NN1",
-                "farmName": "Bondegården",
-                "address": {
-                    "street": "Markvejen 2",
-                    "city": "Bondeby",
-                    "zip": "8123"
-                },
-                "comment": "Ring før høst",
-                "sampleDensity": 1,
-                "samePlanAsLast": true,
-                "takeOwnSamples": true,
-                "area": 100
-            },
-            {
-                "consultant": "NL",
-                "signedDate": new Date("1/1/2017"),
-                "landlineNumber": "88888888",
-                "phoneNumber": "20202020",
-                "name": "NN1",
-                "farmName": "Bondegården",
-                "address": {
-                    "street": "Markvejen 12",
-                    "city": "TestAArhus",
-                    "zip": "8000"
-                },
-                "comment": "Ring under høst",
-                "sampleDensity": 1,
-                "samePlanAsLast": true,
-                "takeOwnSamples": true,
-                "area": 100
-            },
-            {
-                "consultant": "NL",
-                "signedDate": new Date("1/1/2017"),
-                "landlineNumber": "88888888",
-                "phoneNumber": "20202020",
-                "name": "NN1",
-                "farmName": "Bondegården",
-                "address": {
-                    "street": "Markvejen 12",
-                    "city": "Aarhus",
-                    "zip": "8000"
-                },
-                "comment": "Ring under høst",
-                "sampleDensity": 1,
-                "samePlanAsLast": true,
-                "takeOwnSamples": true,
-                "area": 100
-            },
-            {
-                "consultant": "NK",
-                "signedDate": new Date("1/1/2017"),
-                "landlineNumber": "88888888",
-                "phoneNumber": "20202020",
-                "name": "NN1",
-                "farmName": "Bondegården",
-                "address": {
-                    "street": "Markvejen 3",
-                    "city": "Bondeby",
-                    "zip": "8123"
-                },
-                "comment": "Ring under høst",
-                "smapleDensity": 1,
-                "samePlanAsLast": true,
-                "takeOwnSamples": true,
-                "area": 100
-            }
-        ];
-
-        //TODO populate db with dummy consultant, admin and 2 regular consultants
-        //TODO populate db with orders references both regular consultants
-
-        await Season.createSeason("2017/2018");
 
     });
 
@@ -346,7 +201,177 @@ describe("consultant tests", () => {
     describe('Testing delete consultant', function () {
 
         it('should delete a consultant ' +
-            'and update all orders referencing this consultant to reference the dummy consultant instead');
+            'and update all orders referencing this consultant to reference the dummy consultant instead', function () {
+
+            // setup for this test
+            const consultDataArr = [
+                {
+                    "name": "superuser",
+                    "password": "5up3r53cr37P455w0rd",
+                    "isAdmin": true,
+                    "dummy": false
+                },
+                {
+                    "name": "admin",
+                    "password": "admin",
+                    "isAdmin": false,
+                    "dummy": false
+                },
+                {
+                    "name": "Slettet bruger",
+                    "password": "dummy",
+                    "isAdmin": false,
+                    "dummy": true
+                },
+                {
+                    "name": "MH",
+                    "password": "MHPassword",
+                    "isAdmin": false,
+                    "dummy": false
+                },
+                {
+                    "name": "MJ",
+                    "password": "MJPassword",
+                    "isAdmin": false,
+                    "dummy": false
+                },
+                {
+                    "name": "NL",
+                    "password": "NLPassword",
+                    "isAdmin": false,
+                    "dummy": false
+                },
+                {
+                    "name": "NK",
+                    "password": "NKPassword",
+                    "isAdmin": false,
+                    "dummy": false
+                }
+            ];
+
+            const orderDataArr = [
+                {
+                    "consultant": "MH",
+                    "signedDate": new Date("1/1/2017"),
+                    "landlineNumber": "88888888",
+                    "phoneNumber": "20202020",
+                    "name": "NN1",
+                    "farmName": "Bondegården",
+                    "address": {
+                        "street": "Markvejen 1",
+                        "city": "Bondeby",
+                        "zip": "8123"
+                    },
+                    "comment": "Ring efter høst",
+                    "sampleDensity": 1,
+                    "samePlanAsLast": true,
+                    "takeOwnSamples": true,
+                    "area": 100
+                },
+                {
+                    "consultant": "MJ",
+                    "signedDate": new Date("1/1/2017"),
+                    "landlineNumber": "88888889",
+                    "phoneNumber": "20202020",
+                    "name": "NN1",
+                    "farmName": "Bondegården",
+                    "address": {
+                        "street": "Markvejen 2",
+                        "city": "Bondeby",
+                        "zip": "8123"
+                    },
+                    "comment": "Ring før høst",
+                    "sampleDensity": 1,
+                    "samePlanAsLast": true,
+                    "takeOwnSamples": true,
+                    "area": 100
+                },
+                {
+                    "consultant": "NL",
+                    "signedDate": new Date("1/1/2017"),
+                    "landlineNumber": "88888888",
+                    "phoneNumber": "20202020",
+                    "name": "NN1",
+                    "farmName": "Bondegården",
+                    "address": {
+                        "street": "Markvejen 12",
+                        "city": "TestAArhus",
+                        "zip": "8000"
+                    },
+                    "comment": "Ring under høst",
+                    "sampleDensity": 1,
+                    "samePlanAsLast": true,
+                    "takeOwnSamples": true,
+                    "area": 100
+                },
+                {
+                    "consultant": "NL",
+                    "signedDate": new Date("1/1/2017"),
+                    "landlineNumber": "88888888",
+                    "phoneNumber": "20202020",
+                    "name": "NN1",
+                    "farmName": "Bondegården",
+                    "address": {
+                        "street": "Markvejen 12",
+                        "city": "Aarhus",
+                        "zip": "8000"
+                    },
+                    "comment": "Ring under høst",
+                    "sampleDensity": 1,
+                    "samePlanAsLast": true,
+                    "takeOwnSamples": true,
+                    "area": 100
+                },
+                {
+                    "consultant": "NK",
+                    "signedDate": new Date("1/1/2017"),
+                    "landlineNumber": "88888888",
+                    "phoneNumber": "20202020",
+                    "name": "NN1",
+                    "farmName": "Bondegården",
+                    "address": {
+                        "street": "Markvejen 3",
+                        "city": "Bondeby",
+                        "zip": "8123"
+                    },
+                    "comment": "Ring under høst",
+                    "smapleDensity": 1,
+                    "samePlanAsLast": true,
+                    "takeOwnSamples": true,
+                    "area": 100
+                }
+            ];
+
+            //TODO populate db with dummy consultant, admin and 2 regular consultants
+            //TODO populate db with orders references both regular consultants
+
+            Order.remove({});
+            Season.remove({});
+            Consultant.remove({});
+
+            Season.createSeason("2017/2018");
+
+            consultDataArr.forEach(async (element) => {
+                //console.log(element);
+                await Consultant.createConsultant(element)
+                    .then(function (result) {
+                        console.log("Consultant saved.");
+                    })
+                    .catch(function (error) {
+                        console.error(error);
+                    });
+            });
+
+            Consultant.find({})
+                .then(function (result) {
+                    result.forEach(function (element) {
+                        console.log(element);
+                    })
+                })
+                .catch(function (error) {
+                    console.error(error);
+                });
+        });
     });
 
 });
